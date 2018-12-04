2c1eb01be0b043c09b45dc2db223ff4d95eef89e
unknown <yhzhutao94@163.com>
2018-12-04 09:42:12 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app" class="border">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style lang="sass">
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+
+.border
+  border: 1px solid #e3e3e3
+
+
+@media screen and (-webkit-min-device-pixel-ratio: 2)
+  .border
+    border: 0.5px solid #e3e3e3
+
+
+@media screen and (-webkit-min-device-pixel-ratio: 3)
+  .border
+    border: 0.333333px solid #e3e3e3
+
+
+.border_bottom
+  border-bottom: 1px solid #e3e3e3
+
+
+@media screen and (-webkit-min-device-pixel-ratio: 2)
+  .border_bottom
+    border-bottom: 0.5px solid #e3e3e3
+
+
+
+@media screen and (-webkit-min-device-pixel-ratio: 3)
+  .border_bottom
+    border-bottom: 0.333333px solid #e3e3e3
+
+
+
+.border_top
+  border-bottom: 1px solid #e3e3e3
+
+
+@media screen and (-webkit-min-device-pixel-ratio: 2)
+  .border_top
+    border-bottom: 0.5px solid #e3e3e3
+
+
+
+@media screen and (-webkit-min-device-pixel-ratio: 3)
+  .border_top
+    border-bottom: 0.333333px solid #e3e3e3
+
+
+</style>
