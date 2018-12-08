1de8390ec997d44dc447dc794a820a7dd6a1adc7
yhzhu <yhhzutao94@163.com>
2018-12-08 14:03:10 +0800

    indicator component

---
+++ src/components/indicator/indicator.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="indicator">
+      <div class="indicator">
+        <div class="content">
+          <span class="top">{{percent}}%</span>
+          <div class="line"></div>
+          <span class="bottom" :style="{color:fontcolor}">{{index}}</span>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "indicator",
+      props:{
+        fontcolor:String,
+        percent:Number,
+        index:String
+      }
+    }
+</script>
+
+<style lang="scss" scoped>
+#indicator{
+  .indicator{
+    position: relative;
+    width: 260px;
+    height: 260px;
+    border-radius: 50%;
+    box-sizing: border-box;
+    background: #fff;
+    border: 12px solid #dadfec;
+    text-align: center;
+    .content{
+      position: absolute;
+      display: inline-block;
+      width: 80%;
+      margin: 0 auto;
+      top: 50%;
+      transform: translate(-50%,-50%);
+      .top{
+        font-size: 44px;
+      }
+      .line{
+        border-top: 2px #adadad solid;
+        width: 100%;
+        margin: 16px 0;
+      }
+    }
+  }
+}
+</style>
--- src/components/variousIndicators/variousIndicators.vue
+++ src/components/variousIndicators/variousIndicators.vue
@@ -1,12 +1,17 @@
 <template>
     <div class="variousIndicators">
       各项指标界面
+      <v-indicator :fontcolor="'red'" :percent="53.75" :index="'不良率'"></v-indicator>
     </div>
 </template>
 
 <script>
+  import Indicator from '@/components/indicator/indicator'
     export default {
-        name: "variousIndicators"
+        name: "variousIndicators",
+      components:{
+          'v-indicator':Indicator
+      }
     }
 </script>
 
